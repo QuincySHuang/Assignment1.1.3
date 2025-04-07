761bec47e49228697f962565105294293b5f2546
Quincy <qsyhuang@gmail.com>
2025-04-07 14:49:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int num1  = 0;
+int num2 = 0;
+
+Console.WriteLine("Let's divide two numbers now!");
+num1 = Convert.ToInt32(Console.ReadLine());
+num2  = Convert.ToInt32(Console.ReadLine());
+
+int quotient = num1 / num2;
+int mod = num1 % num2;
+
+Console.WriteLine("The quotient is: " + quotient);
+Console.WriteLine("The remainder is: " + mod);
+
